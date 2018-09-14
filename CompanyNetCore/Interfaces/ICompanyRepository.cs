e859be4f6d6dacb46442e2b9d95155f414796295
Nick Essing <nick.essing@tobit.software>
2018-09-14 17:31:51 +0200

    dependency injection

---
+++ CompanyNetCore/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using FirmaTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaTest.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        List<CompanyData> GetNames();
+    }
+}
